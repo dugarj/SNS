d26b65f0015b9635b5d3b5c804bfd5eedfed4a88
dugarj <jdugar11@gmail.com>
2021-09-02 12:43:39 -0500

    Initial Commit

---
+++ SNS.Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SNS.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNS.Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
